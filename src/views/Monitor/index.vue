21d3e2b3e6dcef5fc9eb514a89a2df80f06ded55
FrankLin23 <linzhangsheng23@163.com>
2022-07-13 21:42:39 +0800

    Update Monitor

--- src/views/Monitor/index.vue
+++ src/views/Monitor/index.vue
@@ -1,9 +1,18 @@
-<template>monitor</template>
+<template>
+  <div class="monitor-container">
+    <n-image class="video" src="http://192.168.0.108:8080/?action=stream" />
+  </div>
+</template>
 
-<script>
-export default {
-  name: "index",
-};
-</script>
+<script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+  .monitor-container {
+    width: 100%;
+    display: flex;
+    align-items: center;
+  }
+  .video {
+    margin: 0 auto;
+  }
+</style>
